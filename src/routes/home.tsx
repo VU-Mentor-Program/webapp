96665e87f859078fd9914df42aaeab47fd5e73fe
KortsH <122207777+KortsH@users.noreply.github.com>
2025-01-29 11:58:40 +0100

    padding

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-800 text-white text-center pr-5 pl-5">
       <Header />
-      <Logo />
-      <h1 className="text-2xl font-bold">Mentor Program</h1>
-      <p className="text-base">Aiming to bring first-year students together 😊</p>
-      <SocialLinks />
+        <Logo />
+          <h1 className="text-2xl font-bold">Mentor Program</h1>
+          <p className="text-base">Aiming to bring first-year students together 😊</p>
+        <SocialLinks />
       <Footer />
     </div>
   );
