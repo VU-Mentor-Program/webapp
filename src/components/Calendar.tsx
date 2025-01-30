a0b27660433c5f56478d1fd31ac169780851a42d
KortsH <122207777+KortsH@users.noreply.github.com>
2025-01-30 18:36:14 +0100

    border radius for the calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -6,7 +6,7 @@ export const Calendar: React.FC = () => {
       <iframe
         title="Mentor Program Agenda"
         src="https://calendar.google.com/calendar/u/0/embed?src=c_ac81bc7a0a4f7a062a5623f650e0086ae298a507bf3af92e6df7083d1f1a3b20@group.calendar.google.com&ctz=Europe/Brussels"
-        style={{ border: 0 }}
+        style={{ border: 0, borderRadius: 10 }}
         width="800"
         height="600"
         frameBorder="0"
