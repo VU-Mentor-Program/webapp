b2ea96fbfdf5558a8c863bb95497d4560dacbb8a
KortsH <122207777+KortsH@users.noreply.github.com>
2025-01-30 14:43:30 +0100

    header links to the homepage now

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
-  return <header className="absolute top-5 left-5 text-lg font-bold text-white">Mentor Program</header>;
+  return (
+    <header className="absolute top-5 left-5 text-lg font-bold">
+      <Link to="/" className="text-white hover:text-gray-300 transition">
+        Mentor Program
+      </Link>
+    </header>
+  );
 };
-
-
