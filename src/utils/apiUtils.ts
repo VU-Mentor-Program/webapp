31dc425017cf376d04a20ce4f2004ac096763185
KortsH <122207777+KortsH@users.noreply.github.com>
2025-01-31 22:46:51 +0100

    i dont like this

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,2 +1,19 @@
-export const GET_API_URL = import.meta.env.VITE_GET_API_URL;
-export const POST_API_URL = import.meta.env.VITE_POST_API_URL;
+// # SHHH dont tell anyone
+// # this is a secret
+// # please never do this yourself
+// # this is a bad idea
+// # but for the sake of this project
+// # we will do it
+// # but please never do this yourself
+
+// # also dont tell my boss
+// # they will be mad
+// # but we need to do this
+// # for the sake of the project
+// # and for the sake of the demo
+// # but please never do this yourself
+
+// # also dont tell my mom
+
+export const GET_API_URL = "https://script.google.com/macros/s/AKfycbzv0awFVkoABOfvu0mygTliki47lhpFpj9zgAZuP0qhqEgbv8iykYMcp2wX0WYhQNJu/exec";
+export const POST_API_URL = "https://script.google.com/macros/s/AKfycbzv0awFVkoABOfvu0mygTliki47lhpFpj9zgAZuP0qhqEgbv8iykYMcp2wX0WYhQNJu/exec";
