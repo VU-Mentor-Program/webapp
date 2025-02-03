be4e27e5a6f686520bd2f3f361ecbf3d4751b494
KortsH <122207777+KortsH@users.noreply.github.com>
2025-02-03 17:06:47 +0100

    margin fix for the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { SocialLinks } from "./social-link";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="text-xs text-white mt-auto p-10 bg-gray-800">
+    <footer className="text-xs text-white mt-auto p-10 bg-gray-800 px-4 md:px-24">
       <div className="flex justify-between items-center">
         <SocialLinks />
         <span>© 2025</span>
