ddf92a8ddee70671ef1cc101c028ba10460090bc
KortsH <122207777+KortsH@users.noreply.github.com>
2025-02-04 15:31:09 +0100

    bug fix on the appscirpts side

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -16,5 +16,5 @@
 // # also dont tell my mom
 
 export const GET_API_URL = "https://script.google.com/macros/s/AKfycbzv0awFVkoABOfvu0mygTliki47lhpFpj9zgAZuP0qhqEgbv8iykYMcp2wX0WYhQNJu/exec";
-export const POST_API_URL = "https://script.google.com/macros/s/AKfycby50Vx_YW8rUNHvsk50n2dgNfMTOjGR1IPBwBgMR-0rA24UbJHEuSS2TXt6YcWuR14E/exec";
+export const POST_API_URL = "https://script.google.com/macros/s/AKfycbxmDTW6IpQlk8hOYKwV_NiKGSnt_YZvxtwWi0Sq092zjB8IGUkmWel7WQ1wKzeSlLpq/exec";
 export const GET_LEADERBOARD_API_URL = "https://script.google.com/macros/s/AKfycbzOQfYivf8uqia26vTV5JYsZEjNDC6izYzNoZ73UXCcu01uTk16Igh9cQcZMaFA8N93hw/exec";
