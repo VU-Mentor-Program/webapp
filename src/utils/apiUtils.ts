15fed1476267a0cfe5e65880abb261bfdf205158
Svhn <sven.dijk@gmail.com>
2025-02-25 15:00:39 +0100

    Api update + some documentation for the APIs

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -15,6 +15,10 @@
 
 // # also dont tell my mom
 
+// These are for games
 export const GET_API_URL = "https://script.google.com/macros/s/AKfycbxNf69_qryacUpU05sRDubbbh5TX7TDWKq2mp7bwfPueEDgQm1MNnhodlI0PJEA4Y69/exec";
 export const POST_API_URL = "https://script.google.com/macros/s/AKfycbyCmZBj0QxkDhtvM5sAc1dGZZkwIzqIkxI7MdOKMMTEHDtt3TfNptpI3PMLNEbEF2c/exec";
-export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbza9DppM_Xt0BWkc1J2U-FKKJXBqfx81r32DyvJA2c22cYmHrnNWEOMv07gxbALRclu3g/exec";
+
+// This is for events
+// npm run deploy
+export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbzesF8tt9mddf3P5MomYMxtvRTZsJJssMFgSPFqtQ6O0k849OYdOAjv3pP0j9mR28WTtA/exec";
