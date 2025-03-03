06584ff9562863c74c12719fe0d2f3a4b55b3809
KortsH <122207777+KortsH@users.noreply.github.com>
2025-03-03 12:18:24 +0100

    centered logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,9 @@
-import logo from "../assets/mp_logo.png";
+import logo from "../assets/black_logo.png";
 
 export const Logo = () => {
   return (
     <div className="flex items-center justify-center rounded-full pt-20">
-      <img className="size-30 mb-5 rounded-full" src={logo} alt="Mentor Program Logo" />
+      <img className="size-30 mb-5 rounded-full bg-white br-50" src={logo} alt="Mentor Program Logo" />
     </div>
   );
 };
