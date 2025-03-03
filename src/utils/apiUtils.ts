58c1c6622b14c708063ab267bd2dc60036316cdd
KortsH <122207777+KortsH@users.noreply.github.com>
2025-03-03 12:46:50 +0100

    new API url

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -17,8 +17,8 @@
 
 // These are for games
 export const GET_API_URL = "https://script.google.com/macros/s/AKfycbxNf69_qryacUpU05sRDubbbh5TX7TDWKq2mp7bwfPueEDgQm1MNnhodlI0PJEA4Y69/exec";
-export const POST_API_URL = "https://script.google.com/macros/s/AKfycbyCmZBj0QxkDhtvM5sAc1dGZZkwIzqIkxI7MdOKMMTEHDtt3TfNptpI3PMLNEbEF2c/exec";
+export const POST_API_URL = "https://script.google.com/macros/s/AKfycbzihb64x7frd-Qcybb71QkRXr0kFexcGguTgiTCCI2bIa-TMXy5Gi_EA0BUqjYG0ow0/exec";
 
-// This is for events
+// This is for events (and the showup leaderboard)
 // npm run deploy
 export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbzesF8tt9mddf3P5MomYMxtvRTZsJJssMFgSPFqtQ6O0k849OYdOAjv3pP0j9mR28WTtA/exec";
