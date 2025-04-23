e5d5b7b74798628bd986e931e2499e6941b61260
Svhn <sven.dijk@gmail.com>
2025-04-23 16:06:33 +0200

    API update

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -21,4 +21,4 @@ export const POST_API_URL = "https://script.google.com/macros/s/AKfycbxyo5AK1sHq
 
 // This is for events (and the showup leaderboard)
 // npm run deploy
-export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbyZs515f98ES2PqMXEwBqmlowXL29MZD2_PJF4yU_pC4XRm7oBDpToGspJo9rUlpA-beg/exec";
+export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbxjdAffaWnnTsRQC7yXR43BJh9L2NVIzZjwNJAMlm7z-DO8L2azqxhSmalYnu8ZOBf9dA/exec";
