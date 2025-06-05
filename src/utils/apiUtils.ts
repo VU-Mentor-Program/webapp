10c1ab5f88cb7708c109a0e750a6dc50e9532197
Svhnn <rocker525@hotmail.nl>
2025-06-05 18:41:37 +0200

    Updated beach day API

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -14,11 +14,10 @@
 // # but please never do this yourself
 
 // # also dont tell my mom
-
 // These are for games
 export const GET_API_URL = "https://script.google.com/macros/s/AKfycbyKGqBwkqIVRofAXq0BIEIrwLNXUE0gn7Ku41IxViQ5LjF6Nc7LjOilVSSn43Pi_7lr/exec";
 export const POST_API_URL = "https://script.google.com/macros/s/AKfycbxyo5AK1sHqOabfZbu6FkK0CLH9j_uWNPfYGkDgekHIoSloNgWxmzN7XQh889xRLvw/exec";
 
 // This is for events (and the showup leaderboard)
 // npm run deploy
-export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbwIDpUMh6Are-ra4QOjEUs87rXaJxvEyovDb-Hm3H5pgMJgQp9rXUz1-jDKSwEl3AJqMQ/exec";
+export const GET_SIGNUP_COUNT_API_URL = "https://script.google.com/macros/s/AKfycbxqkyknKx127k6o3qK8RN4bKljV5myUFzCW4Qd1fZN-8CVC0A04mX6NSyatRiJdGZelFw/exec";
